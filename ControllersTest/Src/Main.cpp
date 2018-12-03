3c9e86286955ce7aa9551d6c66518fcb79b05942
ChaoticWeg <shawn@chaoticweg.cc>
2018-12-03 16:15:12 -0600

    initial commit

---
+++ ControllersTest/Src/Main.cpp
@@ -0,0 +1,9 @@
+#include "Defines.h"
+#include "Window.h"
+
+static CWindow g_cGame;
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	return 0;
+}
